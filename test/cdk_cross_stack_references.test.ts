dff85629b740a4a05c7c9dd77a32ded1cd625a42
Klingspor, Christoph <klingspor@gmx.net>
2025-11-22 12:24:50 +0100

    Initial commit

---
+++ test/cdk_cross_stack_references.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkCrossStackReferences from '../lib/cdk_cross_stack_references-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk_cross_stack_references-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkCrossStackReferences.CdkCrossStackReferencesStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
